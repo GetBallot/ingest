a7be73c3a8e0e670c84703bacdd5c1f214bc95d9
Chiu-Ki Chan <chiuki@users.noreply.github.com>
2018-09-05 21:53:46 -0600

    Create input and output directories if needed

--- run.sh
+++ run.sh
@@ -2,6 +2,14 @@
 
 dir=`dirname $0`
 
+if [ ! -d input ]; then
+  mkdir input
+fi
+
+if [ ! -d output ]; then
+  mkdir output
+fi
+
 cat $dir/scripts/candidates.tsv | while read name docid
 do
   input="$dir/input/${name}.tsv"
